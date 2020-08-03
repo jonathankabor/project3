13a358e236d1f53028b245340cfba5ba3d919f6b
kabor <kabor@LAPTOP-1M107DHK.(none)>
2020-08-03 17:03:05 +0200

    Add delete feature

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -95,4 +95,20 @@ class PinsController extends AbstractController
         ]);
 
     }
+
+    /**
+     * @Route("/pins/{id<[0-9]+>}/delete", name="app_pins_delete", methods="DELETE")
+     * @param Pin $pin
+     * @param EntityManagerInterface $em
+     * @return Response
+     */
+    public function delete(Pin $pin,EntityManagerInterface $em): Response
+    {
+
+       $em->remove($pin);
+       $em->flush();
+
+        return $this->redirectToRoute('app_home');
+
+    }
 }
